5c60ef9f3c968a3504a3ad14d697a24df4d879e9
BlueScreen <quandt2191@gmail.com>
2016-09-05 13:39:07 +0700

    Add via upload
    Send email client smtp

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace EmailClient
+{
+    public partial class Form1 : Form
+    {
+      //  NetworkCredential login;
+        SmtpClient client;
+        MailMessage msg;
+        //string strConnection=@"Data Source=HP\SQLEXPRESS; Initial Catalog=Login; Integrated Security=True";
+        //SqlConnection conn;
+        //SqlCommand command;
+        public static TextBox tb = new TextBox();
+        public static TextBox tb1 = new TextBox();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       private void btnLogin_Click(object sender, EventArgs e)
+        {
+           Main Main = new Main();
+            if (txtUsername.Text == "quandt2191@gmail.com" && txtPassword.Text == "160621qtq") 
+            {
+                this.Hide();
+                Main.Show();
+            }
+           else
+           {
+               MessageBox.Show(" sai Username hoặc Password, xin mời nhập lại");
+           }
+           //SmtpClient client = new SmtpClient(txtSmtp.Text);
+       //    login=new NetworkCredential(txtUsername.Text,txtPassword.Text);
+           //client=new SmtpClient(txtSmtp.Text);
+           //client.Port=Convert.ToInt32(txtPort.Text);
+          
+           //client.Credentials=new System.Net.NetworkCredential(txtUsername.Text,txtPassword);
+          //  client.EnableSsl=true;
+          SmtpClient client=new SmtpClient(txtSmtp.Text);
+                client.Port=587;
+                client.Credentials=new System.Net.NetworkCredential(txtUsername.Text,txtPassword.Text);
+                client.EnableSsl=true;
+                //client.Send();
+          /* If (reader.Read()==true), 
+
+                { 
+                this.Hide();
+                From Main=new Main();
+                Main.Show();
+                }
+                Else
+                {
+                MessageBox.Show(“ban dang nhap khong thanh cong”);
+                txtUsername.Text=””;
+                txtUsername.Text=””;
+                txtUsername.Focus();
+                }*/
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tb = txtUsername;
+            tb1 = txtPassword;
+        }
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailClient
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    SmtpClient client = new SmtpClient("smtp.gmail.com", 465);
+            //    client.Credentials = new NetworkCredential(Form1.tb.Text, Form1.tb1.Text);
+            //    MailMessage msg = new MailMessage();
+            //    msg.To.Add(new MailAddress(txtTo.Text));
+            //    msg.From = new MailAddress(txtFrom.Text);
+            //    msg.Subject = txtSubject.Text;
+            //    msg.Body = txtBody.Text;
+            //    client.EnableSsl = true;
+            //    client.Send(msg);
+
+
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Sorry", "Error");
+            //}
+
+
+
+            MailAddress fromAddress = new MailAddress(Form1.tb.Text, "From Name");
+            MailAddress toAddress = new MailAddress(txtTo.Text, "To Name");
+            string fromPassword = Form1.tb1.Text;
+            string subject = txtSubject.Text;
+            string body = txtBody.Text;
+
+            //khai báo SmtpClient
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+            };
+            // truyền tham số cho message
+            using (MailMessage message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            //gửi mail
+            {
+                smtp.Send(message);
+                MessageBox.Show("Gửi Thành Công!");
+            }
+
+        }
+
+      
+    }
+}
